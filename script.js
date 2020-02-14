bdee74d73aeafe1e4ea7d7452761e989af017a7c
Sarah Vieira <smarievieira@gmail.com>
2020-02-14 16:21:15 -0500

    test

---
+++ script.js
@@ -0,0 +1,78 @@
+// Create an event listener that will trigger slow scroll to first question when "start" button is clicked
+$('.start').click(function () {
+    $('html,body').animate({
+        scrollTop: $(".questions").offset().top
+    },
+        'slow');
+});
+
+// Create an event listener that will trigger slow scroll to next question when each answer is clicked
+for (let i = 1; i <= 3; i++) {
+    if (i <= 2) {
+        $(`.input${i}`).click(function () {
+            $('html,body').animate({
+                scrollTop: $(`.question${i + 1}`).offset().top
+            },
+                'slow');
+        });
+    } else {
+        $(`.input${i}`).click(function () {
+            $('html,body').animate({
+                scrollTop: $(`.submit`).offset().top
+            },
+                'slow');
+        });
+    }
+}
+
+// Submit form:
+$('.submitBtn').on('click', function (event) {
+    event.preventDefault();
+
+    // Goal: obtain user input
+    // (1) Store user's answer in a variable
+    // (2) Obtain "checked" radio option from each question
+    // Recall: radio buttons are grouped by name - select input by name
+
+    const userSelectedTemp = $('input[name=question1]:checked').val();
+    const userSelectedEnvironment = $('input[name=question2]:checked').val();
+    const userSelectedCost = $('input[name=question3]:checked').val();
+
+    // Filter our array based on the user's choice:
+    const userChoice = countries[userSelectedTemp];
+    // The array userChoice contains all cities in cold/hot arrays depending on user's selection
+
+    // Select appropriate suggestion from our 'countries' object
+    for (let index = 0; index < userChoice.length; index++) {
+        // Compare user's selected temp countries (hot/cold, userChoice) with the 'environment' AND 'cost' properties in each corresponding object 
+        if (userChoice[index].environment === userSelectedEnvironment
+            &&
+            userChoice[index].cost === userSelectedCost) {
+            resultOptions.push(userChoice[index].country);
+            // This will add the name of the country to our resultsObject array if its environment property AND cost property corresponds with the user input
+
+            resultPhotos.push(userChoice[index].url);
+            // This will add the photo of the country to our resultsPhotos array if its environment property AND cost property corresponds with the user input
+        }
+        // Result: we have two newly formed arrays (resultOptions and resultPhotos) that contain all of the countries (and photos of) in our original array that pertain to the user's preferences
+    }
+
+    // Make our '.results' section in our HTML visible on our page
+    $('.results').css('display', 'flex')
+
+    // Trigger page to scroll down to '.results' section when submit button is clicked
+    $('html,body').animate({
+        scrollTop: $(".results").offset().top
+    },
+        'slow');
+
+    // Print to the results section: target our HTML '.results' div        
+    // Use the return value from our getRandom() to print
+    const randomIndex = getRandom(resultOptions.length);
+    const source = `url(${resultPhotos[randomIndex]})`;
+
+    $('.randomCountry').html(`${resultOptions[randomIndex]}`);
+    $('.results').css({
+        'backgroundImage': source
+    })
+});
