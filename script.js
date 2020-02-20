18ed5414861a756fe5291d7b90115e833cdeb897
kentaylor92 <ken_seaway@gmail.com>
2020-02-19 19:05:06 -0500

    updated CSS, refactored JS to namespace, added refresh button

--- script.js
+++ script.js
@@ -1,81 +1,59 @@
-$(document).ready(function () {
+const app = {};
 
-    const apiKey = 'qeQmGqx4AA9SDsXie418vjjZAJJuFirS';
-
-    // app.categoryFunction = function(result){
-    //     for(let i = 0; i < 10; i++){
-    //         app.category = result._embedded.event[i].classifications[0].segment;
-    //         console.log(app.category);
-    //         console.log("hello")
-    //     }
-    // }
-
-    // app.init = () => {
-    //     console.log("App has started!");
-    //     app.getInfo();
-    // }; //end app init
-
-    let category;
-    let country;
-
-    let emptyCategoryArray = [];
-    let emptyCountryArray = [];
-
-    let userSelectedCountry;
-    let userSelectedSegment;
-    
-
-    // This function will grab info from oue API based on user input
-    getInfo = function (userCountry, userSegment) {
-        $.ajax({
-            url: `https://app.ticketmaster.com/discovery/v2/events.json?&apikey=${apiKey}`,
-            type: 'GET',
-            dataType: 'json',
-            data: {
-                segmentName: `${userSegment}`,
-                countryCode: `${userCountry}`,
-                size: 20,
-            },
-        }).then(function(result){
-            console.log(result);
-            displayInfo(result);
-        }).catch(function(error){
-            console.log('hello');
-            alert(`Sorry - no events found!`)
-        })
-    }
+app.apiKey = 'qeQmGqx4AA9SDsXie418vjjZAJJuFirS';
 
 
+app.userSelectedCountry;
+app.userSelectedSegment;
 
-    displayInfo = function (data) {
-        $('.eventGrid').empty();
-        $('.resultsHeader').empty();
-        
-        const eventsArray = data._embedded.events;
+app.getInfo = function (userCountry, userSegment) {
+    $.ajax({
+        url: `https://app.ticketmaster.com/discovery/v2/events.json?&apikey=${app.apiKey}`,
+        type: 'GET',
+        dataType: 'json',
+        data: {
+            segmentName: `${userSegment}`,
+            countryCode: `${userCountry}`,
+            size: 40,
+        },
+    }).then(function (result) {
+        console.log(result);
+        app.displayInfo(result);
+    }).catch(function (error) {
+        console.log('hello');
+        alert(`Sorry - no events found!`)
+    })
+}
 
-        let userSelectedCountryText = $('select option:selected').text();
+app.displayInfo = function (data) {
+    $('.eventGrid').empty();
+    $('.resultsHeader').empty();
 
-        let headerToAppend = ` 
+    const eventsArray = data._embedded.events;
+
+    let userSelectedCountryText = $('select option:selected').text();
+
+    let headerToAppend = ` 
                 <h2>your results</h2>
-                <p>You chose <span class="segmentSpan">${userSelectedSegment}</span> in <span class="countrySpan">${userSelectedCountryText}!</span></p>
+                <p>You chose <span class="segmentSpan">${app.userSelectedSegment}</span> in <span class="countrySpan">${userSelectedCountryText}!</span></p>
         `
-        $('.resultsHeader').append(headerToAppend);
+    $('.resultsHeader').append(headerToAppend);
 
-        eventsArray.forEach(function (item) {
-            let title = item.name;
-            let imageArray = item._embedded.attractions[0].images;
-            let date = item.dates.start.localDate;
-            let city = item._embedded.venues[0].city.name;
-            let buyTickets = item.url;
-            let image;
+    eventsArray.forEach(function (item) {
+        let title = item.name;
+        let imageArray = item._embedded.attractions[0].images;
+        let date = item.dates.start.localDate;
+        let city = item._embedded.venues[0].city.name;
+        let buyTickets = item.url;
+        let image;
 
-            imageArray.forEach(function (imageObject) {
-                if (imageObject.width > 500 && imageObject.width < 1000) {
-                    return image = imageObject.url;
-                }
-            });
+        imageArray.forEach(function (imageObject) {
+            if (imageObject.width > 500 && imageObject.width < 1000) {
+                return image = imageObject.url;
+            }
+        });
 
-            const htmlToAppend = `
+        const htmlToAppend = `
             
                 <div class="eventContainer">
                     <div class="imageContainer">
@@ -91,90 +69,70 @@ $(document).ready(function () {
                     </div>
                 </div>
             `
+        $('.eventGrid').append(htmlToAppend);
+    })
+}
 
-            $('.eventGrid').append(htmlToAppend);
-        })
+
+$('.start').click(function () {
+    $('html,body').animate({
+        scrollTop: $(".userInputCountry").offset().top
+    },
+        'slow');
+});
+
+$('select').change(function () {
+    $('html,body').animate({
+        scrollTop: $(".userInputSegment").offset().top
+    },
+        'slow');
+});
+
+
+$('.submitBtn').on('click', function (event) {
+    event.preventDefault();
+
+    app.userSelectedCountry = $("select").val();
+    app.userSelectedSegment = $('input[name=userInputSegment]:checked').val();
+
+    if (app.userSelectedCountry === undefined || app.userSelectedSegment === undefined) {
+        $('.hidden').css('display', 'block');
     }
 
-    // Create an event listener that will trigger slow scroll to first question when "start" button is clicked
-    $('.start').click(function () {
-        $('html,body').animate({
-            scrollTop: $(".userInputCountry").offset().top
-        },
-            'slow');
-    });
+    else {
+        $('.results').css('display', 'block');
+        $('.hidden').css('display', 'none');
 
-    $('select').change(function () {
-        $('html,body').animate({
-            scrollTop: $(".userInputSegment").offset().top
-        },
-            'slow');
-    });
+        console.log('Submit button has been clicked!')
 
-    // Submit form:
-    $('.submitBtn').on('click', function (event) {
-        event.preventDefault();
-        
-        // userSelectedCountry = $('input[name=userInputCountry]:checked').val();
-        // userSelectedCountry = $('select').change(function(){
-        //     ('select option:selected').val();
-        // });
+        let countryCode;
 
-        userSelectedCountry = $("select").val();
-        userSelectedSegment = $('input[name=userInputSegment]:checked').val();
-        
-        if (userSelectedCountry === undefined || userSelectedSegment === undefined){
-            $('.hidden').css('display','block');
-        }
+        app.getInfo(app.userSelectedCountry, app.userSelectedSegment);
 
-        // Goal: obtain user input
-        // (1) Store user's answer in a variable
-        // (2) Obtain "checked" radio option from each question
-        // Recall: radio buttons are grouped by name - select input by name
-
-        else{
-            $('.hidden').css('display', 'none');
-
-            console.log('Submit button has been clicked!')
-    
-            let countryCode;
-    
-            console.log(userSelectedCountry);
-            console.log(userSelectedSegment);
-    
-            // if (userSelectedCountry === "canada") {
-            //     countryCode = "CA";
-            // }
-            // else if (userSelectedCountry === "usa") {
-            //     countryCode = "US";
-            // }
-            // else {
-            //     countryCode = "MX";
-            // }
-    
-            // console.log(countryCode);
-    
-            // getCountryInfo(countryCode);
-            // getSegmentInfo(userSelectedSegment);
-            getInfo(userSelectedCountry, userSelectedSegment);
-    
+        setTimeout(() => {
             $('html,body').animate({
                 scrollTop: $(".results").offset().top
             },
                 'slow');
-        }
+        }, 500);
+    }
 
-    });
+});
 
-    $('.inputConcerts').on('click', function () {
-        $('.userInputSegment').css('background-image', 'url(./assets/concerts.jpg)');
-    })
-    $('.inputSports').on('click', function () {
-        $('.userInputSegment').css('background-image', 'url(./assets/sports.jpg)');
-    })
-    $('.inputArts').on('click', function () {
-        $('.userInputSegment').css('background-image', 'url(./assets/theatre.jpg)');
-    })
+$('.inputConcerts').on('click', function () {
+    $('.userInputSegment').css('background-image', 'url(./assets/concerts.jpg)');
+})
+$('.inputSports').on('click', function () {
+    $('.userInputSegment').css('background-image', 'url(./assets/sports.jpg)');
+})
+$('.inputArts').on('click', function () {
+    $('.userInputSegment').css('background-image', 'url(./assets/theatre.jpg)');
+})
+
+
+
+$(document).ready(function () {
+//   TODO: ask about init and what has to be in it
 
 }); //end doc ready
 
@@ -192,21 +150,3 @@ $(document).ready(function () {
 
 
 
-// Create an event listener that will trigger slow scroll to next question when each answer is clicked
-// for (let i = 1; i <= 3; i++) {
-//     if (i == 1) {
-//         $(`.input${i}`).click(function () {
-//             $('html,body').animate({
-//                 scrollTop: $(`.userInputAttraction`).offset().top
-//             },
-//                 'slow');
-//         });
-//     } else {
-//         $(`.input${i}`).click(function () {
-//             $('html,body').animate({
-//                 scrollTop: $(`.results`).offset().top
-//             },
-//                 'slow');
-//         });
-//     }
-// }
